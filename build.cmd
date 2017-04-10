6b351da0f46c9405a0dedc3a10568aad6d1afbec
hsc <kevin.patrick.schmidt@googlemail.com>
2017-04-10 14:20:13 +0200

    Build angle. (5)

--- build.cmd
+++ build.cmd
@@ -1,21 +1,25 @@
 set PATH=%PATH%;%CD%\depot_tools
-set GYP_GENERATORS=ninja
+set GYP_GENERATORS=msvs
+if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" (set GYP_MSVS_VERSION=2017)
+if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2015" (set GYP_MSVS_VERSION=2015)
+if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2013" (set GYP_MSVS_VERSION=2013)
 
 cd angle
 python scripts/bootstrap.py
 gclient sync
-ninja -C out/Release
+msbuild samples\samples.sln /p:Configuration=Release /p:Platform="x64"
 cd ..
 
-xcopy /E /I %CD%\angle\include\EGL %CD%\include\EGL
-xcopy /E /I %CD%\angle\include\KHR %CD%\include\KHR
-xcopy /E /I %CD%\angle\include\GLES2 %CD%\include\GLES2
-xcopy /E /I %CD%\angle\include\GLES3 %CD%\include\GLES3
+xcopy /E /I angle\include\EGL include\EGL
+xcopy /E /I angle\include\KHR include\KHR
+xcopy /E /I angle\include\GLES2 include\GLES2
+xcopy /E /I angle\include\GLES3 include\GLES3
 
 if exist lib rd /s /q lib
 mkdir lib
-copy %CD%\angle\out\Release\lib\libGLESv2.lib %CD%\lib\libGLESv2.lib
-copy %CD%\angle\out\Release\libEGL.dll %CD%\lib\libEGL.dll
-copy %CD%\angle\out\Release\libGLESv2.dll %CD%\lib\libGLESv2.dll
-copy %CD%\angle\out\Release\d3dcompiler_47.dll %CD%\lib\d3dcompiler_47.dll
+copy angle\samples\Release_x64\lib\libEGL.lib lib\libEGL.lib
+copy angle\samples\Release_x64\lib\libGLESv2.lib lib\libGLESv2.lib
+copy angle\samples\Release_x64\libEGL.dll lib\libEGL.dll
+copy angle\samples\Release_x64\libGLESv2.dll lib\libGLESv2.dll
+copy angle\samples\Release_x64\d3dcompiler_47.dll lib\d3dcompiler_47.dll
 dir lib
