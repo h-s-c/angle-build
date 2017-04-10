03861e05dca098a8f3fafcf9eceb6421530ff013
hsc <kevin.patrick.schmidt@googlemail.com>
2017-04-10 14:24:37 +0200

    Build angle. (7)

--- build.sh
+++ build.sh
@@ -18,6 +18,6 @@ cp -rf angle/include/GLES2/* include/GLES2
 cp -rf angle/include/GLES3/* include/GLES3
 
 mkdir -p lib
-cp -f angle/out/Release/lib/libEGL.so lib
-cp -f angle/out/Release/lib/libGLESv2.so lib
+cp -f angle/out/Release/libEGL.so lib
+cp -f angle/out/Release/libGLESv2.so lib
 ls lib
