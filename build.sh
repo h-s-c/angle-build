96c7e499b62f6e619574949ca933eabb057ed83d
hsc <kevin.patrick.schmidt@googlemail.com>
2017-04-10 03:41:51 +0200

    Build angle. (2)

--- build.sh
+++ build.sh
@@ -23,7 +23,15 @@ gclient sync
 ninja -C out/Release
 cd ..
 
+mkdir -p include/EGL
+mkdir -p include/KHR
+mkdir -p include/GLES2
+mkdir -p include/GLES3
 cp -rf angle/include/EGL/* include/EGL
 cp -rf angle/include/KHR/* include/KHR
 cp -rf angle/include/GLES2/* include/GLES2
 cp -rf angle/include/GLES3/* include/GLES3
+
+mkdir -p lib
+cp -f angle/out/Release/lib/libEGL.so lib
+cp -f angle/out/Release/lib/libGLESv2.so lib
