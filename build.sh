8af3056d9d1993f3ad17bd8d6e38472ca73a4880
hsc <kevin.patrick.schmidt@googlemail.com>
2017-04-10 03:20:33 +0200

    Build angle.

--- build.sh
+++ build.sh
@@ -16,9 +16,14 @@ python --version
 git submodule update --init --recursive
 export PATH=${PWD}/depot_tools:$PATH
 
-export GYP_GENERATORS=msvs
+export GYP_GENERATORS=ninja
 cd angle
 python scripts/bootstrap.py
 gclient sync
 ninja -C out/Release
 cd ..
+
+cp -rf angle/include/EGL/* include/EGL
+cp -rf angle/include/KHR/* include/KHR
+cp -rf angle/include/GLES2/* include/GLES2
+cp -rf angle/include/GLES3/* include/GLES3
